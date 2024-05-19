e86366b26ce074428987ac015952f547f5cd6964
salmanfaiz786 <salman.faiz85@gmail.com>
2024-05-19 12:47:24 +0500

    first JS assignment

---
+++ day1.js
@@ -0,0 +1,33 @@
+
+function printLastName(firstName){
+    
+if(firstName ==="Adeel"){
+    console.log ("Ahmad")
+}
+else if(firstName ==="Salman"){
+    console.log ("Faiz")
+} 
+
+else if(firstName ==="Shoaib"){
+    console.log ("Khalid")
+} 
+
+else if(firstName ==="Khalid"){
+    console.log ("Siddiqui")
+} 
+
+else if(firstName ==="Farrukh"){
+    console.log ("Hussain")
+} 
+else
+{
+    console.log ("Invalid name")
+}
+}
+
+printLastName("Adeel");
+printLastName("Khalid");
+printLastName("Shoaib");
+printLastName("Salman");
+printLastName("Farrukh");
+printLastName("abc");
