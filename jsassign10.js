71c22736093eac3405f10e42c52041d26f4067a5
salmanfaiz786 <salman.faiz85@gmail.com>
2024-05-27 00:58:27 +0500

    JS Assignment no10

---
+++ jsassign10.js
@@ -0,0 +1,9 @@
+let names = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama", "Salman"];
+let nameCount = names.length;
+
+    if (nameCount % 2 == 0) { 
+        console.log("Even"); 
+    } 
+    else { 
+        console.log("Odd");
+    }
