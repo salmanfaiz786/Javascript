eb36f4540f0a180f04567cbf9d407d3d6444a22b
salmanfaiz786 <salman.faiz85@gmail.com>
2024-05-28 03:44:29 +0500

    javascript assignment 11

---
+++ jsassign11.js
@@ -0,0 +1,16 @@
+let names = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama", "Salman", "Khalid"];
+let nameCount = names.length;
+
+function f1(nameCount){
+    if (nameCount % 2 == 0) { 
+        console.log("Your input array is Even"); 
+    } 
+    else { 
+        console.log("Your input array is Odd");
+    }
+
+}
+f1(nameCount);
+
+
+
