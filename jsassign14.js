b6671073171ffddd33c5ea234e8d9246c25b3574
salmanfaiz786 <salman.faiz85@gmail.com>
2024-06-12 18:43:33 +0500

    Playwright assignment 20

--- jsassign14.js
+++ jsassign14.js
@@ -1,4 +1,4 @@
-function countUniqueNumbers(searchItem) {
+/*function countUniqueNumbers(searchItem) {
     let str = "00011100001111";
     let count = 0;
     for (let i = 0; i < str.length; i++) {
@@ -19,5 +19,5 @@ if(resultOnes > resultZeros){
 else if(resultZeros > resultOnes){
 
     console.log("No of 0's are greater");
-}
+}*/
 
